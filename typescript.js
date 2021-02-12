464a531bafc088dee078cdaa6e8dea8b686cf078
Alexander Patin <alex@squarelink.com>
2021-02-12 15:46:29 -0500

    Fixed es6/typescript interop

--- typescript.js
+++ typescript.js
@@ -1,22 +1,37 @@
 module.exports = {
-  extends: [
-    './base.js',
-    'standard-with-typescript',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-    'plugin:import/typescript',
+  extends: ['./base.js'],
+  overrides: [
+    {
+      env: { browser: true, es6: true, node: true },
+      extends: [
+        './base.js',
+        'standard-with-typescript',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+        'plugin:import/typescript',
+      ],
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2020,
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+      plugins: ['prettier', '@typescript-eslint'],
+      rules: {
+        '@typescript-eslint/semi': ['error'],
+        '@typescript-eslint/strict-boolean-expressions': 'off',
+        'jsdoc/require-param-type': 'off',
+        'prettier/prettier': [
+          'error',
+          {
+            semi: false,
+            singleQuote: true,
+          },
+        ],
+        semi: 'off',
+      },
+    },
   ],
-  ignorePatterns: ['tsconfig.json'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020,
-    project: './tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['prettier'],
-  rules: {
-    '@typescript-eslint/strict-boolean-expressions': 'off',
-    'jsdoc/require-param-type': 'off',
-  },
 }
